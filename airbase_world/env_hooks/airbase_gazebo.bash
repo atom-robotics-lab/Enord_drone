264a417db4f30b819044f223d20c695c24b71bb7
NOEMOJI041 <aloksharma041@gmail.com>
2023-06-20 22:32:17 +0530

    setup fix bash

--- airbase_world/env_hooks/airbase_gazebo.bash
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-source /usr/share/gazebo/setup.bash
-export GAZEBO_MODEL_PATH="${CATKIN_ENV_HOOK_WORKSPACE}/../src/aribase_world/models/:${GAZEBO_MODEL_PATH}"
-export GAZEBO_RESOURCE_PATH="${CATKIN_ENV_HOOK_WORKSPACE}/../src/airbase_world/models/:${CATKIN_ENV_HOOK_WORKSPACE}/../src/airbase_world/worlds/:${GAZEBO_RESOURCE_PATH}"
