9e7ff217daca8de32153c4050005586f04b1d116
NOEMOJI041 <aloksharma041@gmail.com>
2023-06-24 20:02:17 +0530

    placed aruco marker correctly and multiple uav

--- pushpak_description/scripts/aruco_detector.py
+++ pushpak_description/scripts/aruco_detector.py
@@ -12,26 +12,50 @@ class Robot_Controller:
     def __init__(self):
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/iris/camera/rgb/image_raw", Image, self.callback)
-        self.image_pub = rospy.Publisher("image_topic_2", Image)
-        self.aruco_detection = rospy.Publisher("/aruco_detection", Twist, queue_size=10)
+        self.image_sub = rospy.Subscriber("/iris0/camera/rgb/image_raw", Image, self.callback0)
+        self.image_sub = rospy.Subscriber("/iris1/camera/rgb/image_raw", Image, self.callback1)
+        self.image_sub = rospy.Subscriber("/iris2/camera/rgb/image_raw", Image, self.callback2)
 
-        self.aruco_msg = Twist()
+        self.image_pub = rospy.Publisher("image_topic_2", Image)
+        self.aruco_detection0 = rospy.Publisher("/aruco_detection0", Twist, queue_size=10)
+        self.aruco_detection1 = rospy.Publisher("/aruco_detection1", Twist, queue_size=10)
+        self.aruco_detection2 = rospy.Publisher("/aruco_detection2", Twist, queue_size=10)
+
+
+
+        self.aruco_msg0 = Twist()
+        self.aruco_msg1 = Twist()
+        self.aruco_msg2 = Twist()
 
         self.id = None
        
         self.detect = detection()
         self.detect.T = 3
 
-    def publish(self, x_error, y_error):
-        self.aruco_msg.linear.x = x_error
-        self.aruco_msg.linear.y = y_error
-        self.aruco_detection.publish(self.aruco_msg)
+    def publish0(self, x_error0, y_error0):
+        self.aruco_msg0.linear.x = x_error0
+        self.aruco_msg0.linear.y = y_error0
+        self.aruco_detection0.publish(self.aruco_msg0)
 
-    def callback(self, data):
-        self.cv1_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    def publish1(self, x_error0, y_error0):
+        self.aruco_msg1.linear.x = x_error0
+        self.aruco_msg1.linear.y = y_error0
+        self.aruco_detection1.publish(self.aruco_msg1)
+
+    def publish2(self, x_error0, y_error0):
+        self.aruco_msg2.linear.x = x_error0
+        self.aruco_msg2.linear.y = y_error0
+        self.aruco_detection2.publish(self.aruco_msg2)
+
+    def callback0(self, data):
+        self.cv1_image0 = self.bridge.imgmsg_to_cv2(data, "bgr8")
         self.control_loop()
-
+    def callback1(self, data):
+        self.cv1_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.control_loop()
+    def callback2(self, data):
+        self.cv1_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.control_loop()
 
     def direction(self, markerID):
 
@@ -45,52 +69,127 @@ class Robot_Controller:
     def control_loop(self):
         
         
-        self.Result = self.detect.aruco_detection(self.cv1_image)
-        aruco_center, aruco_radius = self.Result[1] , self.Result[2]
+        self.Result0 = self.detect.aruco_detection(self.cv1_image0)
+        aruco_center0, aruco_radius0 = self.Result0[1] , self.Result0[2]
+        self.Result1 = self.detect.aruco_detection(self.cv1_image1)
+        aruco_center1, aruco_radius1 = self.Result1[1] , self.Result1[2]
+        self.Result2 = self.detect.aruco_detection(self.cv1_image2)
+        aruco_center2, aruco_radius2 = self.Result2[1] , self.Result2[2]
 
-        frame_height = self.Result[0].shape[0]
-        frame_width  = self.Result[0].shape[1]
-        x_error = 0
-        y_error = 0
+        frame_height0 = self.Result0[0].shape[0]
+        frame_width0  = self.Result0[0].shape[1]
+        frame_height1 = self.Result1[0].shape[0]
+        frame_width1  = self.Result1[0].shape[1]
+        frame_height2 = self.Result2[0].shape[0]
+        frame_width2  = self.Result2[0].shape[1]
+        x_error0 = 0
+        y_error0 = 0
+        x_error1 = 0
+        y_error1 = 0
+        x_error2 = 0
+        y_error2 = 0
 
-        if aruco_center != None and aruco_radius != None:
-            aruco_x = aruco_center[0]
-            aruco_y = aruco_center[1]
+        if aruco_center0 != None and aruco_radius0 != None:
+            aruco_x0 = aruco_center0[0]
+            aruco_y0 = aruco_center0[1]
 
 
 
-            if (aruco_x > frame_height + 10):
+            if (aruco_x0 > frame_height0 + 10):
+                print("Go Right")
+                x_error0 = 1
+            elif(aruco_x0 < frame_height0 - 10):
+                print("Go Left")
+                x_error0 = -1
+            else:
+                x_error0 = 0
+            
+            if (aruco_y0 > 300):
+                print("Go Down")
+                y_error = -1
+            elif (aruco_y0 < 280):
+                print("Go Up")
+                y_error0 = 1
+            else:
+                y_error0 = 0
+            
+            self.publish0(x_error0, y_error0)
+        else:
+            x_error = 5
+            y_error = 0
+            self.publish0(x_error0, y_error0)
+        if aruco_center1 != None and aruco_radius1 != None:
+            aruco_x1 = aruco_center1[0]
+            aruco_y1 = aruco_center1[1]
+
+
+
+            if (aruco_x1 > frame_height1 + 10):
                 print("Go Right")
                 x_error = 1
-            elif(aruco_x < frame_height - 10):
+            elif(aruco_x1 < frame_height1 - 10):
+                print("Go Left")
+                x_error1 = -1
+            else:
+                x_error1 = 0
+            
+            if (aruco_y1 > 300):
+                print("Go Down")
+                y_error1 = -1
+            elif (aruco_y1 < 280):
+                print("Go Up")
+                y_error1 = 1
+            else:
+                y_error1 = 0
+            
+            self.publish1(x_error1, y_error1)
+        else:
+            x_error1 = 5
+            y_error1 = 0
+            self.publish1(x_error1, y_error1)
+
+        if aruco_center2 != None and aruco_radius2 != None:
+            aruco_x2 = aruco_center2[0]
+            aruco_y2 = aruco_center2[1]
+
+
+
+            if (aruco_x2 > frame_height2 + 10):
+                print("Go Right")
+                x_error = 1
+            elif(aruco_x2 < frame_height2 - 10):
                 print("Go Left")
                 x_error = -1
             else:
                 x_error = 0
             
-            if (aruco_y > 300):
+            if (aruco_y2 > 300):
                 print("Go Down")
                 y_error = -1
-            elif (aruco_y < 280):
+            elif (aruco_y2 < 280):
                 print("Go Up")
-                y_error = 1
+                y_error2 = 1
             else:
-                y_error = 0
+                y_error2 = 0
             
-            self.publish(x_error, y_error)
+            self.publish2(x_error2, y_error2)
         else:
-            x_error = 5
-            y_error = 0
-            self.publish(x_error, y_error)
+            x_error2 = 5
+            y_error2 = 0
+            self.publish2(x_error2, y_error2)
 
-        cv2.line(self.Result[4] , (frame_height,0),(frame_height,frame_width),(255,0,0),2)
-        cv2.line(self.Result[4] , (0, 290),(1000, 290),(255,0,0),2)
-
-            
+        cv2.line(self.Result0[4] , (frame_height0,0),(frame_height0,frame_width0),(255,0,0),2)
+        cv2.line(self.Result0[4] , (0, 290),(1000, 290),(255,0,0),2)
+        cv2.line(self.Result1[4] , (frame_height1,0),(frame_height1,frame_width1),(255,0,0),2)
+        cv2.line(self.Result1[4] , (0, 290),(1000, 290),(255,0,0),2)
+        cv2.line(self.Result2[4] , (frame_height2,0),(frame_height2,frame_width2),(255,0,0),2)
+        cv2.line(self.Result2[4] , (0, 290),(1000, 290),(255,0,0),2)           
         #cv2.putText(self.Result[4], self.lt, (300, 800), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
         #cv2.putText(self.Result[4], self.at, (350, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
 
-        cv2.imshow("Frame", self.Result[4])
+        cv2.imshow("Frame0", self.Result0[4])
+        cv2.imshow("Frame1", self.Result1[4])
+        cv2.imshow("Frame2", self.Result2[4])
         cv2.waitKey(1)
 
 
