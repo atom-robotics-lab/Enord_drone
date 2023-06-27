44c5b7143a45349e93515076be81e46c64a3a9d7
Kartik9250 <kartikrana9250@gmail.com>
2023-06-27 19:41:03 +0530

    Added comments to aruco_detector.py and detector.py

--- drone_controller/src/aruco_detector.py
+++ drone_controller/src/aruco_detector.py
@@ -1,94 +1,123 @@
 #! /usr/bin/env python3
+
+# Import necessary libraries and packages
 import rospy
 from geometry_msgs.msg import Twist
 from detector import detection
-from cv_bridge import CvBridge, CvBridgeError
-
-import message_filters
-
+from cv_bridge import CvBridge
 import cv2
 from sensor_msgs.msg import Image
 
 
+'''The Robot_Controller class will receive the camera feed from the drones, detect the aruco markers and publish
+the position errors of the aruco markers in a topic'''
+
 class Robot_Controller:
     def __init__(self):
-
+        ''' This is the constructor function where variable and object initialization will take place'''
+        
+        # Initializing CvBridge() for converting sensor_msgs/msg/Image data to numpy matrices
         self.bridge = CvBridge()
+
+        # Initializing subscribers to obtain camera feed from the drones
         self.image_sub0 = rospy.Subscriber("/iris0/camera/rgb/image_raw", Image, callback=self.callback0)
         self.image_sub1 = rospy.Subscriber("/iris1/camera/rgb/image_raw", Image, callback=self.callback1)
         self.image_sub2 = rospy.Subscriber("/iris2/camera/rgb/image_raw", Image, callback=self.callback2)
 
+        # Initializing publishers to publish position errors
         self.aruco_detection0 = rospy.Publisher("/aruco_detection0", Twist, queue_size=10)
         self.aruco_detection1 = rospy.Publisher("/aruco_detection1", Twist, queue_size=10)
         self.aruco_detection2 = rospy.Publisher("/aruco_detection2", Twist, queue_size=10)
 
-
-
+        # Initializing empty Twist() messages
         self.aruco_msg0 = Twist()
         self.aruco_msg1 = Twist()
         self.aruco_msg2 = Twist()
 
         self.id = None
-       
-        self.detect = detection()
-        self.detect.T = 3
 
-    def image_cb(self, data1, data2, data3):
-        print("test")
-        self.cv1_image0 = self.bridge.imgmsg_to_cv2(data1, "bgr8")
-        self.cv1_image1 = self.bridge.imgmsg_to_cv2(data2, "bgr8")
-        self.cv1_image2 = self.bridge.imgmsg_to_cv2(data3, "bgr8")
-        self.control_loop()
+        # Initialize the aruco detector
+        self.detect = detection()
 
     def publish0(self, x_error0, y_error0):
+        ''' This function is used to publish position error for iris0'''
+
+        # Initialize x and y errors
         self.aruco_msg0.linear.x = x_error0
         self.aruco_msg0.linear.y = y_error0
+
+        # Publish data 
         self.aruco_detection0.publish(self.aruco_msg0)
 
     def publish1(self, x_error1, y_error1):
+        ''' This function is used to publish position error for iris1'''
+
+        # Initialize x and y errors
         self.aruco_msg1.linear.x = x_error1
         self.aruco_msg1.linear.y = y_error1
+
+        # Publish data        
         self.aruco_detection1.publish(self.aruco_msg1)
 
     def publish2(self, x_error2, y_error2):
+
+        ''' This function is used to publish position error for iris1'''
+
+        # Initialize x and y errors
         self.aruco_msg2.linear.x = x_error2
         self.aruco_msg2.linear.y = y_error2
+
+        # Publish data      
         self.aruco_detection2.publish(self.aruco_msg2)
 
     def callback0(self, data):
+        '''Callback function for camera feed from iris1'''
+
+        # Convert sensor_msgs/msg/Image datatype to numpy matrix
         self.cv1_image0 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
     def callback1(self, data):
+        '''Callback function for camera feed from iris1'''
+
+        # Convert sensor_msgs/msg/Image datatype to numpy matrix
         self.cv1_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        self.control_loop()
+    
     def callback2(self, data):
+        '''Callback function for camera feed from iris1'''
+
+        # Convert sensor_msgs/msg/Image datatype to numpy matrix
         self.cv1_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
- 
 
-    def direction(self, markerID):
+        # Execute the control_loop()
+        self.control_loop()
 
-        if markerID == 1:
-            return 0.3, "Turning Left ", "ID - 1"
-        elif markerID == 2:
-            return -0.3, "Turning Right", "ID - 2"
-        else:
-            return 0, "", "Parked" , "ID - 3"
 
     def control_loop(self):
-  
+        '''This is the main control loop for the detection of aruco markers
+        and calculation of position errors of the aruco markers w.r.t the drones
+        '''
+
+        # Get the aruco detection results for iris0
         self.Result0 = self.detect.aruco_detection(self.cv1_image0)
         aruco_center0, aruco_radius0 = self.Result0[1] , self.Result0[2]
+
+        # Get the aruco detection results for iris1
         self.Result1 = self.detect.aruco_detection(self.cv1_image1)
         aruco_center1, aruco_radius1 = self.Result1[1] , self.Result1[2]
+
+        # Get the aruco detection results for iris2
         self.Result2 = self.detect.aruco_detection(self.cv1_image2)
         aruco_center2, aruco_radius2 = self.Result2[1] , self.Result2[2]
 
-
+        # Store the height and width of camera frames
         frame_height0 = self.Result0[0].shape[0]
         frame_width0  = self.Result0[0].shape[1]
         frame_height1 = self.Result1[0].shape[0]
         frame_width1  = self.Result1[0].shape[1]
         frame_height2 = self.Result2[0].shape[0]
         frame_width2  = self.Result2[0].shape[1]
+
+        # Initialize x and y errors as zero
         x_error0 = 0
         y_error0 = 0
         x_error1 = 0
@@ -96,12 +125,15 @@ class Robot_Controller:
         x_error2 = 0
         y_error2 = 0
 
+        # Publish position error for iris0 using values from aruco detector
         if aruco_center0 != None and aruco_radius0 != None:
+            
+            # Centers of the detected arco markers
             aruco_x0 = aruco_center0[0]
             aruco_y0 = aruco_center0[1]
 
-
-
+            # x_error is 1 when aruco is to the right of the vertical center of
+            # frame, -1 when aruco is to the left and zero otherwise
             if (aruco_x0 > frame_height0 + 10):
                 print("Go Right")
                 x_error0 = 1
@@ -111,6 +143,8 @@ class Robot_Controller:
             else:
                 x_error0 = 0
             
+            # y_error is 1 when aruco is to the top of the horizontal center of
+            # frame, -1 when aruco is below and zero otherwise
             if (aruco_y0 > 300):
                 print("Go Down")
                 y_error0 = -1
@@ -122,15 +156,19 @@ class Robot_Controller:
             
             self.publish0(x_error0, y_error0)
         else:
+
+            # Publish x_error=5 if aruco is not visible
             x_error0 = 5
             y_error0 = 0
             self.publish0(x_error0, y_error0)
+
+        # Publish position error for iris1 using values from aruco detector
         if aruco_center1 != None and aruco_radius1 != None:
             aruco_x1 = aruco_center1[0]
             aruco_y1 = aruco_center1[1]
 
-
-
+            # x_error is 1 when aruco is to the right of the vertical center of
+            # frame, -1 when aruco is to the left and zero otherwise
             if (aruco_x1 > frame_height1 + 10):
                 print("Go Right")
                 x_error1 = 1
@@ -140,6 +178,8 @@ class Robot_Controller:
             else:
                 x_error1 = 0
             
+            # y_error is 1 when aruco is to the top of the horizontal center of
+            # frame, -1 when aruco is below and zero otherwise
             if (aruco_y1 > 300):
                 print("Go Down")
                 y_error1 = -1
@@ -151,16 +191,19 @@ class Robot_Controller:
             
             self.publish1(x_error1, y_error1)
         else:
+
+            # Publish x_error=5 if aruco is not visible
             x_error1 = 5
             y_error1 = 0
             self.publish1(x_error1, y_error1)
 
+        # Publish position error for iris2 using values from aruco detector
         if aruco_center2 != None and aruco_radius2 != None:
             aruco_x2 = aruco_center2[0]
             aruco_y2 = aruco_center2[1]
 
-
-
+            # x_error is 1 when aruco is to the right of the vertical center of
+            # frame, -1 when aruco is to the left and zero otherwise
             if (aruco_x2 > frame_height2 + 10):
                 print("Go Right")
                 x_error2 = 1
@@ -170,6 +213,8 @@ class Robot_Controller:
             else:
                 x_error2 = 0
             
+            # y_error is 1 when aruco is to the top of the horizontal center of
+            # frame, -1 when aruco is below and zero otherwise
             if (aruco_y2 > 300):
                 print("Go Down")
                 y_error2 = -1
@@ -181,19 +226,21 @@ class Robot_Controller:
             
             self.publish2(x_error2, y_error2)
         else:
+
+            # Publish x_error=5 if aruco is not visible
             x_error2 = 5
             y_error2 = 0
             self.publish2(x_error2, y_error2)
 
+        # Draw horizontal and vertical center lines in the frame
         cv2.line(self.Result0[4] , (frame_height0,0),(frame_height0,frame_width0),(255,0,0),2)
         cv2.line(self.Result0[4] , (0, 290),(1000, 290),(255,0,0),2)
         cv2.line(self.Result1[4] , (frame_height1,0),(frame_height1,frame_width1),(255,0,0),2)
         cv2.line(self.Result1[4] , (0, 290),(1000, 290),(255,0,0),2)
         cv2.line(self.Result2[4] , (frame_height2,0),(frame_height2,frame_width2),(255,0,0),2)
         cv2.line(self.Result2[4] , (0, 290),(1000, 290),(255,0,0),2)           
-        #cv2.putText(self.Result[4], self.lt, (300, 800), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
-        #cv2.putText(self.Result[4], self.at, (350, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
 
+        # Display the camera frames
         cv2.imshow("Frame0", self.Result0[4])
         cv2.imshow("Frame1", self.Result1[4])
         cv2.imshow("Frame2", self.Result2[4])
@@ -202,11 +249,17 @@ class Robot_Controller:
 
 
 def main():
+
+    # Initialize the ROS node
     rospy.init_node("aruco_detector_node", anonymous=True)
+    
+    # Initialize the Robot_Controller class
     of = Robot_Controller()
     try:
         rospy.spin()
     except:
         print("error")
 
+
+# Start the ROS node
 main()
--- drone_controller/src/detector.py
+++ drone_controller/src/detector.py
@@ -1,33 +1,49 @@
 # ! /usr/bin/env python3
 
-import imutils
+# Import necessary libraries
 import cv2
 import math
+import imutils
 
+# Intialize the parameters for aruco detection using OpenCV
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
-
 arucoParams = cv2.aruco.DetectorParameters_create()
 
+'''The detection class is a helper class for the aruco_detector_node, used to detect 
+aruco markers from input camera feed using OpenCV'''
 
 class detection():
     def __init__(self):
+        '''This is the constructor function where the  variables and objects will get initialized'''
 
         self.center = None
         self.markerID1 = None
         self.radius1 = None
-        self.T = 0
 
     def aruco_detection(self, image):
-
+        '''This is the main function which receives the image and detects aruco markers'''
         self.image = image
+
+        # Convert the image to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+        # Resize the image
         image = imutils.resize(image, width=1000)
+        
+        # Use the OpenCV to detect the aruco markers and retreive their co-ordinates and ids
         (corners, ids, rejected) = cv2.aruco.detectMarkers(image,
                                                            arucoDict, parameters=arucoParams)
+
+        # Proceed further if aruco markers are detected in the input image       
         if len(corners) > 0:
+
+            # Retreive the individual ids
             ids = ids.flatten()
+
+            # Retreive the co-ordinates and centers of the individual markers 
             for (markerCorner, markerID) in zip(corners, ids):
+                
+                # Calculate the corners of the aruco markers
                 corners = markerCorner.reshape((4, 2))
                 (topLeft, topRight, bottomRight, bottomLeft) = corners
                 topRight = (int(topRight[0]), int(topRight[1]))
@@ -35,36 +51,27 @@ class detection():
                 bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
                 topLeft = (int(topLeft[0]), int(topLeft[1]))
                 
+                # Calculate the radius of the aruco marker
                 radius = int(math.sqrt(
                     (int(topRight[0]) - int(bottomLeft[0])) ** 2 + (int(topRight[1]) - int(bottomLeft[1])) ** 2) / 2)
+                
+                # Calculate the center co-ordinates of the detected aruco markers
                 cX = int((topLeft[0] + bottomRight[0]) / 2.0)
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                
+                # Draw centers around the aruco markers
                 cv2.circle(image, (cX, cY), radius, (0, 0, 255), 3)
                 cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
-                cv2.putText(image, "Aruco Marker ID = " + str(markerID),
-                            (topLeft[0] + 20, topLeft[1] - 55),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.5, (0, 0, 0), 2)
-
               
-                self.center = (((topLeft[0] + bottomRight[0]) / 2.0), ((topLeft[1] + bottomRight[1]) / 2.0))
-                # print(self.center)
+                self.center = (cX, cY)
                 self.markerID1 = markerID
-
-                # print(self.markerID1)
                 self.radius1 = radius
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             exit()
 
+        # Return aruco detection results
         return [gray, self.center, self.radius1, self.markerID1, image]
 
-# if __name__== "__main__" :
-#
-#     det = detection()
-#     det1 = det.aruco_detection(cv2.imread("/home/ayan/trying.png"))
-#
-#     cv2.imshow("gray" , det1[0])
-#     print(det1[1])
-#     cv2.waitKey(0)
+
