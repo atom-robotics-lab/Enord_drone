01980b57b46a2eb8f116edfd5c1e8f0dae35ca22
NOEMOJI041 <aloksharma041@gmail.com>
2023-06-27 18:22:07 +0530

    created setup.bash for PX4 setup

---
+++ setup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv ~/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/iris ~/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/iris1
+cp pushpak_description/models/iris ~/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/ -r
